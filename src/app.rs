fc4fc8548525ff40c6ce3d98f4f53ac96ff133f4
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-12-26 10:01:41 +0100

    types

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,9 @@
-use eframe::egui::{self, Id};
-use special_relativity;
+use eframe::{egui::{self, Id, Painter}, egui_glow::painter, epaint::{Pos2, Color32}};
+use special_relativity::{self, Spacetime};
 
 pub struct SRApp {
     rng: rand::rngs::ThreadRng,
+    spacetime: Spacetime,
     c: f32,
 }
 
@@ -10,7 +11,19 @@ impl SRApp {
     pub fn new() -> Self {
         Self {
             rng: rand::thread_rng(),
+            spacetime: Spacetime::new(),
             c: 10.0,
+        }
+    }
+}
+
+fn draw(app: &SRApp, painter: &Painter) {
+    let r = 20.0;
+    let color = Color32::from_rgb(128, 128, 128);
+    for frame in app.spacetime.frames.iter() {
+        for position in frame.positions.iter() {
+            let screen = Pos2::new(position.x, position.y);
+            painter.circle_filled(screen, r, color);
         }
     }
 }
@@ -18,7 +31,7 @@ impl SRApp {
 impl eframe::App for SRApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::SidePanel::right("control_panel").show(ctx, |ui| {
-            egui::Slider::new(&mut self.c, 1.0..=1000.0).logarithmic(true).text("c");
+            ui.add(egui::Slider::new(&mut self.c, 1.0..=1000.0).logarithmic(true).text("c"));
             ui.separator();
         });
 
@@ -26,6 +39,9 @@ impl eframe::App for SRApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // compute time step
             let real = ui.input(|i| i.unstable_dt).min(1.0 / 30.0);
+
+            draw(self, ui.painter());
+
         });
     }
 }
