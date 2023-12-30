8644526b60408f76f636588a3bd170f5fcd14cc3
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-12-30 20:55:47 +0100

    control ship

--- src/app.rs
+++ src/app.rs
@@ -1,31 +1,78 @@
-use eframe::{egui::{self, Id, Painter}, egui_glow::painter, epaint::{Pos2, Color32}};
-use special_relativity::{self, Spacetime};
+use eframe::{egui::{self, Id, Painter, Key}, epaint::{Pos2, Color32, Vec2, PathShape, Stroke}};
+use nalgebra::{Rotation2, Vector2, Point2};
+use special_relativity::{self, Position, Frame, Velocity};
+
+struct Transform {
+    
+}
+
+impl Transform {
+    fn new() -> Transform {
+        Transform {}
+    }
+    fn forward(&self, position: &Point2<f32>) -> Pos2 {
+        Pos2::new(position.x, position.y) + Vec2::new(320.0, 200.0)
+    }
+}
 
 pub struct SRApp {
     rng: rand::rngs::ThreadRng,
-    spacetime: Spacetime,
+    ship: Ship,
     c: f32,
+    transform: Transform,
+}
+
+struct Ship {
+    frame: Frame,
+    angle: f32,
 }
 
 impl SRApp {
     pub fn new() -> Self {
         Self {
             rng: rand::thread_rng(),
-            spacetime: Spacetime::new(),
+            ship: Ship {
+                frame: Frame::new(Position::new(0.0, 0.0, 0.0), Velocity::new(0.0, 0.0, 1.0)),
+                angle: 0.0,
+            },
             c: 10.0,
+            transform: Transform::new(),
+        }
+    }
+
+    fn control_ship(&mut self, i: &egui::InputState) {
+        if i.key_down(Key::ArrowLeft) {
+            self.ship.angle -= 0.1;
+        }
+        if i.key_down(Key::ArrowRight) {
+            self.ship.angle += 0.1;
+        }
+        if i.key_down(Key::ArrowUp) {
+            
         }
     }
 }
 
-fn draw(app: &SRApp, painter: &Painter) {
-    let r = 20.0;
+
+fn draw(app: &SRApp, painter: &Painter, transform: &Transform) {
+    ///let r = 20.0;
     let color = Color32::from_rgb(128, 128, 128);
-    for frame in app.spacetime.frames.iter() {
-        for position in frame.positions.iter() {
-            let screen = Pos2::new(position.x, position.y);
-            painter.circle_filled(screen, r, color);
-        }
-    }
+    //let position = app.ship.frame.position;
+    //let screen = transform.forward(&position);
+    //painter.circle_filled(screen, r, color);
+    let points = vec![
+        Point2::new(0.0, -10.0),
+        Point2::new(5.0, 10.0),
+        Point2::new(-5.0, 10.0),
+    ];
+    let rotation = Rotation2::new(app.ship.angle);
+    let ship_shape = PathShape {
+        points: points.iter().map(|p| transform.forward(&(rotation * p))).collect(),
+        closed: false,
+        fill: color,
+        stroke: Stroke::NONE,
+     };
+    painter.add(ship_shape);
 }
 
 impl eframe::App for SRApp {
@@ -39,9 +86,8 @@ impl eframe::App for SRApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // compute time step
             let real = ui.input(|i| i.unstable_dt).min(1.0 / 30.0);
-
-            draw(self, ui.painter());
-
+            draw(self, ui.painter(), &self.transform);
+            ui.input(|i| self.control_ship(i));
         });
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,18 @@ use nalgebra::{Vector3, Matrix, U3, ArrayStorage};
 
 // 2D-space time three-vector. x,y and time (z). Time cordinates are stored
 // divided by c
-type Position = Vector3<f32>; 
-type Velocity = Vector3<f32>;
+pub type Position = Vector3<f32>; 
+pub type Velocity = Vector3<f32>;
 
 pub struct Frame {
-    pub positions: Vec<Position>,
+    pub position: Position,
     pub velocity: Velocity,
 }
 
-pub struct Spacetime {
-    pub metric: Metric,
-    pub frames: Vec<Frame>,  // All reference frames
-    reference: usize, // The reference in use
+impl Frame {
+    pub fn new(position: Position, velocity: Velocity) -> Frame {
+        Frame {position, velocity }
+    }
 }
 
 pub type Matrix3x3<T> = Matrix<T, U3, U3, ArrayStorage<T, 3, 3>>;
@@ -25,26 +25,3 @@ const MINKOWSKI: Metric = Metric::new(
     0.0, 1.0, 0.0,
     0.0, 0.0, -1.0,
 );
-
-impl Spacetime {
-    pub fn new() -> Spacetime {
-        let mut frames = Vec::new();
-        frames.push(Frame{
-            positions: Vec::new(),
-            velocity: Velocity::new(0.0, 0.0, 1.0),
-        });
-        Spacetime {
-            metric: MINKOWSKI,
-            frames: frames,
-            reference: 0,
-        }
-    }
-}
-
-pub fn step(spacetime: &mut Spacetime, dt: f32) {
-    for frame in spacetime.frames.iter_mut() {
-        for position in frame.positions.iter_mut() {
-            *position += frame.velocity * dt;
-        }
-    }
-}
